b4fdcb5f78a9f590745c5d8d312274b1ad1b2bf2
Md. Ashraful Hossen Akash <ashrafulhossainakash@gmail.com>
2026-01-10 15:15:32 +0600

    Add files via upload

---
+++ 03_slicing.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+array = np.array([[1,2,3,4],
+                 [5,6,7,8],
+                 [9,10,11,12],
+                 [13,14,15,16]])
+
+# array [start:end:step]
+
+print(array[2,3])
+print(array[-2])
+
+#slice operator, row selection
+
+print(array[1:4])
+print(array[0:4])
+print(array[:2])
+print(array[::2])
+print(array[::-1])
+print(array[::-2])
+
+#column selection
+
+print(array[:,1])
+print(array[:,1:4])
+print(array[:,::-2])
+
+print(array[0:2,0:4:3])
